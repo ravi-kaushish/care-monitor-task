778e373ff01cff88e35e072975d03a0eb22a5add
Ravi Kant Sharma <ravi.sharma@cginfinity.com>
2024-08-06 22:50:19 +0530

    Added Support to override global config via env vars

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 8001;
 const apiVersion = process.env.API_VERSION || 'v1';
 
 // Using bodyparser middleware
-server.use(require('body-parser').json({limit: '50mb'}));
+server.use(require('body-parser').json({ limit: `${ process.env.REQUEST_MAX_BODY_SIZE_MB || 5 }mb` }));
 
 // Application health endpoint
 server.get('/', (req, res) => {
-  res.send('Hello, your app is up and running!')
+  res.send('Hello, your app is up and running!');
 });
 
 // Service discovery map endpoint
@@ -23,7 +23,7 @@ server.use(`/api/${apiVersion}/clinical-data`, require('./routes/clinical-data')
 
 // App starts from here
 server.listen(port, () => {
-  console.log(`Server is started on port ${port}`)
+  console.log(`Server is started on port ${port}`);
 });
 
 module.exports = server;
