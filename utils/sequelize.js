969ee2da7baff56ebe482bfcd358f3faeed2cbf9
Ravi Kant Sharma <ravi.sharma@cginfinity.com>
2024-08-07 02:30:42 +0530

    Added Connection Pooling for db connection resuse

--- utils/sequelize.js
+++ utils/sequelize.js
@@ -1,10 +1,19 @@
 const Sequelize = require('sequelize')
 
+var connection = null
+
 exports.ExecuteQuery = async (query = '', bind = {}) => {
   try {
-    const connection = new Sequelize(process.env.DATABASE_URL, {
-      logging: process.env.DATABASE_LOGGING || false
-    })
+    if (!connection) {
+      connection = new Sequelize(process.env.DATABASE_URL, {
+        logging: process.env.DATABASE_LOGGING || false
+      })
+      // Refresh connections every 1 hour
+      setInterval(() => {
+        connection = null
+        console.log(`Refreshing database connection...`)
+      }, (process.env.DATABASE_CONNECTION_REFRESH_TIME || 1) * 60 * 60 * 1000)
+    }
     if (!query) {
       return {
         error: true,
