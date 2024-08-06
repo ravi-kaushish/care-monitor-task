200855fea1e1a6b8c90d07738432d237196d8d17
Ravi Kant Sharma <ravi.sharma@cginfinity.com>
2024-08-07 02:31:12 +0530

    Updated Database migration scripts

--- utils/db-migrations.js
+++ utils/db-migrations.js
@@ -4,24 +4,45 @@ exports.RunSchemaMigrations = async () => {
   console.log('**** Running DB Migrations ****')
   scripts = [
     {
-      table_name: 'patients',
+      table_name: 'organisations',
       ddl: `CREATE TABLE organisations (
-	    org_id varchar(40) primary key,
-	    name varchar(50) not null,
-	    is_active boolean not null default true,
-	    registered_at TIMESTAMPTZ not null default NOW()
-    )`,
+        org_id varchar(40) primary key,
+        name varchar(50) not null,
+        is_active boolean not null default true,
+        registered_at TIMESTAMPTZ not null default NOW()
+      );`,
       binds: {}
     },
     {
       table_name: 'patients',
       ddl: `CREATE TABLE patients (
-      patient_id varchar(40) primary key,
-      org_id varchar(40) references organisations(org_id),
-      name VARCHAR(100),
-      gender CHAR(1),
-      created_at TIMESTAMPTZ not null default NOW()
-    );`,
+        patient_id varchar(40) primary key,
+        org_id varchar(40) references organisations(org_id),
+        name VARCHAR(100),
+        gender CHAR(1),
+        created_at TIMESTAMPTZ not null default NOW()
+      );`,
+      binds: {}
+    },
+    {
+      table_name: 'measurements',
+      ddl: `CREATE TABLE measurements (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    unit VARCHAR(10),
+    key varchar(100),
+    keywords varchar(255)
+);`,
+      binds: {}
+    },
+    {
+      table_name: 'metrics',
+      ddl: `CREATE TABLE metrics (
+        id SERIAL primary key,
+        measurement_id int references measurements(id),
+        value varchar(20),
+        timestamp timestamptz not null default NOW()
+      );`,
       binds: {}
     }
   ]
@@ -33,10 +54,14 @@ exports.RunSchemaMigrations = async () => {
       if (!re.error) {
         console.log(`Created Table ${script.table_name}`)
       } else {
-        console.log(`Error occured while executing DLL for Table ${script.table_name}`)
+        console.log(
+          `Error occured while executing DLL for Table ${script.table_name}, ${re.error}`
+        )
       }
-    }else{
-      console.log(`Table ${script.table_name} already exists, skipping migration.`)
+    } else {
+      console.log(
+        `Table ${script.table_name} already exists, skipping migration.`
+      )
     }
   }
 }
