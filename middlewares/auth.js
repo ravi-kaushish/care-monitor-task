69e88d30fcd35b3b308792046ea12a0d455dc034
Ravi Kant Sharma <ravi.sharma@cginfinity.com>
2024-08-07 16:17:29 +0530

    Sample Middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,14 @@
-const { SECURITY_CONFIG, VerifyJWT } = require("../../utils/server/security");
-
 //middleware to Verify User Identity
 exports.Authorize = async (req, res, next) => {
     if (req.headers["authorization"]) {
-        const result = await VerifyJWT(req.headers["authorization"].replace("Bearer ", ""), SECURITY_CONFIG.jwt.secret);
-        if (!result.error) return next();
-        res.statusCode = 401;
-        res.send("Invalid Access Token");
+        // Your Authorization Logic here
+
+        // if (authorized) return next();
+
+        // res.statusCode = 401;
+        // res.send("Invalid Access Token");
+
+        return next()
     } else {
         res.statusCode = 401
         res.send("Unauthorized");
