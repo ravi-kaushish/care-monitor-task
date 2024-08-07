ad0c590a4d5a9deff543e9e6b07be2d9160cf192
Ravi Kant Sharma <ravi.sharma@cginfinity.com>
2024-08-07 16:10:56 +0530

    Updated clinical data APIs

--- controllers/clinical-data.js
+++ controllers/clinical-data.js
@@ -1,22 +1,61 @@
-const { ExecuteQuery } = require('../utils/sequelize')
-const { aggregateHeartRateData } = require('../utils/aggregate')
-
-exports.GetClinicalData = async (req, res) => {
-  let result = await ExecuteQuery('select * from temple', {})
-  if (result.error) {
-    res.status(500).send('Error Occured While Executing Database Query')
-    return
-  }
-  let [data, metadata] = result.data
-  res.send(data)
-}
+const { GetMeasurements, AggregateHeartRateData } = require('../utils/aggregate')
+const { SaveMetrics } = require('../utils/ingest')
 
 exports.IngestClinicalData = async (req, res) => {
-  let aggregatedDate = await aggregateHeartRateData(req.body.clinical_data.HEART_RATE.data);
-  res.send(aggregatedDate)
+  res.status(202).send("Your request to ingest clinical data has been accepted.");
+  this.SaveDataToDB(req.body);
 }
 
 exports.ProcessClinicalData = async (req, res) => {
-  let aggregatedDate = await aggregateHeartRateData(req.body.clinical_data.HEART_RATE.data);
-  res.send(aggregatedDate)
+  // Processing Req and sending aggregated response data
+  const aggregatedHeartRateData = await AggregateHeartRateData(req.body.clinical_data.HEART_RATE.data);
+  res.send(aggregatedHeartRateData);
+  // Saving Data to the Database
+  this.SaveDataToDB(req.body);
+}
+
+// Sample Implementation for heart rate data ingestion
+exports.IngestHeartRateData = async (req, res) => {
+  res.status(202).send("Your request to ingest heart rate data has been accepted.");
+  // Continue to ingest heart rate data
+}
+
+// Sample Implementation for heart rate data processing
+exports.ProcessHeartRateData = async (req, res) => {
+// Process Heart Rate Data and send response
+ res.send({
+  data: ["Processed heart rate data"]
+ });
+}
+
+exports.SaveDataToDB = async __ => {
+  const result = await GetMeasurements();
+  const org_id = __.orgId
+  const patient_id = __.patient_id
+  clinical_data = __.clinical_data
+  if (!result.error) {
+    let [measurements] = result.data
+    Object.keys(clinical_data).forEach(key => {
+      //Only Process to save the data when there is data in the clinical data object
+      if (clinical_data[key] && clinical_data[key].data && clinical_data[key].data.length) {
+        
+        // Get the Measurement for current metric
+        measurement = measurements.find(a => a.key === key)
+        
+        //Saving the Metrics to the database
+        SaveMetrics(patient_id, measurement, clinical_data[key].data);
+        
+        // Extra
+        // If you want different transformations with each type of metric data before saving to DB, 
+        // do the trasformation before calling the savemetrics, or create different implementations of savemetrics like below
+            // if (key === 'HEART_RATE') {
+            //   SaveHeartRateMetrics(patient_id, measurement, clinical_data[key].data)
+            // } else if (key === 'WEIGHT') {
+            //   SaveWeightMetrics(patient_id, measurement, clinical_data[key].data)
+            // }...
+      }
+    })
+  } else {
+    //Log The request, push it into a queue or a cache to process it later
+  }
 }
--- routes/clinical-data.js
+++ routes/clinical-data.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
-const { GetClinicalData, IngestClinicalData, ProcessClinicalData } = require("../controllers/clinical-data");
+const {  IngestClinicalData, ProcessClinicalData } = require("../controllers/clinical-data");
 
-// All method support for Odata Service
-router.get("/composite/ingest", GetClinicalData);
+// Endpoint just to Save Data
 router.post("/composite/ingest", IngestClinicalData);
+
+// Endpoint to Process and Save Data
 router.post("/composite/process", ProcessClinicalData);
+
+//  Create Multiple Implementations of API for each type of health data
+// router.post("/heart-rate/ingest", IngestHeartRateData);
+// router.post("/heart-rate/process", ProcessHeartRateData);
 
 module.exports = router;
